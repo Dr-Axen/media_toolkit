0915cd44f33f2e7f07d90069a5e68abcfc3c1579
Dr-Axen <draxen@gmail.com>
2022-12-31 09:35:53 -0500

    Added getopts to audio script, output file + stdout messages

--- make_audio_catalog.sh
+++ make_audio_catalog.sh
@@ -9,12 +9,14 @@
 # It outputs a CSV-formatted data.                      #
 # Requires "tone" and "ffprobe"                         #
 #########################################################
+# return values from functions are opposite of what normal bash/unix 
+# exit codes are. They are boolean Yes=1/No=0 instead.
+# This seemed more logical.
 
 # function to extract cover image from an audio file
 extract_cover() {
 	file="$*"
 	if [ "x$file" == "x" ]; then
-	  echo Need file name
 	  return 0
 	fi
 	codec=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of csv=s=,:p=0 -sexagesimal "$file")
@@ -30,14 +32,14 @@ extract_cover() {
 	    return 0
 	    ;;
 	esac
-	ffmpeg -i "$file" -an -c:v copy cover.$ext 
+	ffmpeg -i "$file" -an -c:v copy cover.$ext
+	echo "Extracted '"$ext"' image" 
   return 1
 }
 
 # function to check files for an embedded cover
 check_embedded_cover() {
 	if [ -z "$*" ]; then
-	  echo Need directory name
 	  return 1
 	else 
 	  dir="$*"
@@ -59,7 +61,6 @@ check_embedded_cover() {
 # function to check for a cover image in specific folder
 check_cover() {
 	if [ -z "$*" ]; then
-	  echo Need directory name
 	  return 0
 	else 
 	  dir="$*"
@@ -70,11 +71,13 @@ check_cover() {
 	# check if the file is an image
 	is_image=$(file "$cover" | grep -w image | wc -l)
 	if [[ $is_image -gt 0 ]]; then
+	  echo "Cover file exists"
 	  return 1
 	fi
 	# if any of the audio files have embedded cover art, extract it
 	check_embedded_cover "$dir"
 	if  [[ $? -gt 0 ]]; then 
+	  echo "Embedded cover exists"
 	  return 1
 	fi
 	# try to find cover image with a different name
@@ -82,6 +85,7 @@ check_cover() {
 	# check if the file is an image
 	is_image="$(file "alt_$cover" | grep -w image | wc -l)"
 	if [[ $is_image -gt 0 ]]; then
+	  echo "Found alernate cover file"
 	  # copy the file to cover.<ext>
 	  ext="${alt_cover##*.}"
 	  cp "$alt_cover" "cover.$ext"
@@ -92,6 +96,7 @@ check_cover() {
 	# hopefully it's a cover image, fingers crossed!
 	is_image=$(file "$image" | grep -w image | wc -l)
 	if [[ $is_image -gt 0 ]]; then
+	  echo "Found image file, fingers crossed"
 	  # copy the file to cover.<ext>
 	  ext="${image##*.}"
 	  cp "$image" "cover.$ext"
@@ -105,15 +110,16 @@ check_cover() {
 check_audio_meta () {
 	if [ -z "$*" ]; then
 	  echo Need directory name
-	  return 1
+	  return 0
 	else 
 	  dir="$*"
 	fi
 	# do not show stderr
 	exec 2> /dev/null
-	# check if the folder has files in it
+	# check if the folder has files in it, besides just cover and description
 	has_files=$(find "$dir" -maxdepth 1 -type f \( ! -name 'desc.txt' -and ! -name 'cover.*' \) -type f -print | wc -l )
 	if [[ $has_files -gt 0 ]]; then
+	  echo "Analyzing '"$dir"'"
 	  # Booksonic expects album cover in "cover.jpg" or "cover.png"
 	  has_cover=check_cover "$dir"
 	  # Booksonic expects album description in "desc.txt"
@@ -148,21 +154,41 @@ check_audio_meta () {
 	  if [[ "$meta_artist" != "$author" ]]; then
 	    author_matches_meta=0
 	  fi
+	  echo "Found '"$dir"' with the following metadata artist='"$meta_artist"', album='"$meta_album"', comment='"$meta_comment"'"
+	  echo "Has cover: ["$has_cover"], Has desc: ["$has_desc"], Has reader: ["$has_reader"]"
 	  # output the result as CSV data line
-	  echo '"'$dir'","'$author'","'$meta_artist'","'$series'","'$title'","'$meta_album'","'$meta_comment'",'$author_matches_meta','$has_cover','$has_desc','$has_reader
+	  echo '"'$dir'","'$author'","'$meta_artist'","'$series'","'$title'","'$meta_album'","'$meta_comment'",'$author_matches_meta','$has_cover','$has_desc','$has_reader >> "$output"
 	fi
-	return 0
+	return 1
 }
-
+########################################################################
 # main program starts here
+########################################################################
 export -f check_audio_meta
-if [ -z "$*" ]; then
-  echo Need directory name
-  exit
-else 
-  start_dir="$*"
-fi
+# set initial parameter values
+start_dir="."
+output="audio_catalog.csv"
+# read parameters
+while getopts ":d:o:" opt; do
+  case $opt in
+    d)
+      start_dir="$OPTARG"
+      ;;
+    o)
+      output="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+echo "Starting program in '"$start_dir"', output file is '"$output"'"
 # print header
-echo '"path","author","meta_artist","series","title","meta_album","meta_comment","author_matches_meta","embedded_cover","has_images","has_cover","has_desc","has_reader"'
+echo '"path","author","meta_artist","series","title","meta_album","meta_comment","author_matches_meta","embedded_cover","has_images","has_cover","has_desc","has_reader"' > "$output"
 # find all directories and check them
 find "$start_dir" -type d -exec bash -c 'check_audio_meta "$0"' "{}" \; 
